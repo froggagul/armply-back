dc559fa0c215452d0bd7c9cf35081c5bcf6ed010
froggagul <froggagul@gmail.com>
2020-08-05 00:01:33 +0900

    error solve

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 
 export function checkAuthenticated(req: Request, res: Response, next: NextFunction) {
-  console.log(req.isAuthenticated);
+  console.log(req.isAuthenticated());
   if (!req.isAuthenticated()) {
     res.status(401).json({reason: 'UNAUTHENTICATED'});
   } else {
