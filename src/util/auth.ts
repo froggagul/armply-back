3e25122128fcda41248ad071cd85fe24752b23c3
froggagul <froggagul@gmail.com>
2020-08-05 00:00:36 +0900

    df

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 
 export function checkAuthenticated(req: Request, res: Response, next: NextFunction) {
+  console.log(req.isAuthenticated);
   if (!req.isAuthenticated()) {
     res.status(401).json({reason: 'UNAUTHENTICATED'});
   } else {
