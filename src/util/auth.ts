f447c40c874e962f340974616953bfa3ac4e1b19
froggagul <froggagul@gmail.com>
2020-08-05 01:23:29 +0900

    checking user

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export function checkAuthenticated(req: Request, res: Response, next: NextFunction) {
   console.log(req.isAuthenticated());
+  console.log(req.user);
   if (!req.isAuthenticated()) {
     res.status(401).json({reason: 'UNAUTHENTICATED'});
   } else {
